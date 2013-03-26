4320219f154eecc9221c505514f1b58ba664dcbd
Zach Colon <zach.colon@tma1.com>
2013-03-26 16:37:10 -0400

    fixed multiple file upload issue

--- spec/dummy/app/controllers/accounts_controller.rb
+++ spec/dummy/app/controllers/accounts_controller.rb
@@ -10,12 +10,15 @@ class AccountsController < ApplicationController
   def update
     @account = Account.find(params[:id])
     @account.attributes = params[:account]
-    @doc = @account.modulate_documents.detect(&:new_record?)
-    @doc.attached_by_id = 1 unless @doc.blank?
+    @docs = @account.modulate_documents.select(&:new_record?)
+    @docs.each do |doc|
+      doc.attached_by_id = 1 unless doc.blank?
+    end
+
     if @account.save
       redirect_to accounts_path, notice: "successfully"
     else
-      flash.now[:alert] = "I like pie Error"
+      flash.now[:alert] = "error"
       render :show
     end
   end
