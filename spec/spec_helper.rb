7985249aeaafa4bdf962e1fcf35341ea525a6b88
Zach Colon <zach.colon@tma1.com>
2013-04-05 10:10:26 -0400

    added riak test server

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,14 +9,31 @@ require "rspec/rails"
 require "factory_girl_rails"
 require "launchy"
 require "pry"
+require 'riak/test_server'
+require 'yaml'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-  
-  config.order = :random
+CarrierWave.configure do |config|
+  config.storage = :riak
+  config.riak_nodes = [
+    { :host => "127.0.0.1", :http_port => 9000 }
+  ]
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+
+    config.order = :random
+
+    begin
+      config = YAML.load_file('spec/support/test_server.yml')
+      $riak_test_server = Riak::TestServer.create(config.symbolize_keys)
+      $riak_test_server.start
+    rescue => e
+      warn "Can't run Riak::TestServer specs. Specify the location of your Riak installation in 'spec/support/test_server.yml. See warning e.inspect"
+    end
+  end
 end
