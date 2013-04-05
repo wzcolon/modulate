5cf0b169cfda347b78d435622c1f27c7969fcb23
Zach Colon <zach.colon@tma1.com>
2013-04-05 10:47:04 -0400

    Use risk test server in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,17 +17,22 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.order = :random
+end
+
 CarrierWave.configure do |config|
   config.storage = :riak
   config.riak_nodes = [
     { :host => "127.0.0.1", :http_port => 9000 }
   ]
+end
 
-  RSpec.configure do |config|
-    config.mock_with :rspec
+RSpec.configure do |config|
 
-    config.order = :random
-
+  config.before :suite do
     begin
       config = YAML.load_file('spec/support/test_server.yml')
       $riak_test_server = Riak::TestServer.create(config.symbolize_keys)
@@ -35,5 +40,13 @@ CarrierWave.configure do |config|
     rescue => e
       warn "Can't run Riak::TestServer specs. Specify the location of your Riak installation in 'spec/support/test_server.yml. See warning e.inspect"
     end
+  end
+
+  config.after :suite do
+    FileUtils.rm_rf(File.expand_path('../../uploads', __FILE__))
+    $riak_test_server.stop
+    puts "Stoping test server..." 
+    $riak_test_Server = nil
+    FileUtils.rm_rf(File.expand_path('../test_server', __FILE__))
   end
 end
